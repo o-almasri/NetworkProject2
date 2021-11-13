2bf4dd8cf58f81d0c683cceb06dc75585f6c1cb6
hfonseca2177 <hfonseca21@gmail.com>
2021-11-12 23:27:55 -0300

    Fix configurations and builds

--- Server/PacketManager.cpp
+++ Server/PacketManager.cpp
@@ -17,10 +17,8 @@ PacketManager* PacketManager::GetInstance()
 		instance->handlerMap.insert(std::make_pair(0, new PacketSendMessageHandler()));
 		instance->handlerMap.insert(std::make_pair(2, new PacketJoinRoomHandler()));
 		instance->handlerMap.insert(std::make_pair(3, new PacketLeaveRoomHandler()));
-		instance->handlerMap.insert(std::make_pair(4, new cPacketRegisterHandler()));
-		instance->handlerMap.insert(std::make_pair(5, new cPacketLoginHandler()));
-		////instance->handlerMap.insert(std::make_pair(6, new cPacketRegisterResponseHandler()));
-		////instance->handlerMap.insert(std::make_pair(7, new cPacketLoginResponseHandler()));
+		/*instance->handlerMap.insert(std::make_pair(4, new cPacketRegisterHandler()));
+		instance->handlerMap.insert(std::make_pair(5, new cPacketLoginHandler()));*/
 	}
 
 	return instance;
